687f7965a6c981a1a7a2b4ed603c4b8a74af946d
Andrew R. <alramirez1@pima.edu>
2026-04-13 16:27:34 -0700

    version update

--- web-tom-upgrade.sh
+++ web-tom-upgrade.sh
@@ -6,7 +6,7 @@
 # Make sure to set desired version and User (i.e. tomcat)
 
 # Tomcat version to install
-TOMCAT_VERSION=9.0.116
+TOMCAT_VERSION=9.0.117
 
 # Update URL for new versions
 SATELLITE_URL="https://satellite6-prod.pima.edu/pulp/content/Pima_Community_College/Library/custom/Tomcat/Tomcat-9/apache-tomcat-$TOMCAT_VERSION.tar.gz"
